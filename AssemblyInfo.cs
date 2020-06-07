ba57871c065dc95b385fae8951bcefe23c437ffb
Herp Derpinstine <herpderpinstine@gmail.com>
2020-06-06 20:34:53 -0500

    Add files via upload

---
+++ AssemblyInfo.cs
@@ -0,0 +1,20 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using MelonLoader;
+
+[assembly: AssemblyTitle(AvatarStatsShowAuthor.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(AvatarStatsShowAuthor.BuildInfo.Company)]
+[assembly: AssemblyProduct(AvatarStatsShowAuthor.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + AvatarStatsShowAuthor.BuildInfo.Author)]
+[assembly: AssemblyTrademark(AvatarStatsShowAuthor.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(AvatarStatsShowAuthor.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(AvatarStatsShowAuthor.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonModInfo(typeof(AvatarStatsShowAuthor.AvatarStatsShowAuthor), AvatarStatsShowAuthor.BuildInfo.Name, AvatarStatsShowAuthor.BuildInfo.Version, AvatarStatsShowAuthor.BuildInfo.Author, AvatarStatsShowAuthor.BuildInfo.DownloadLink)]
+[assembly: MelonModGame("VRChat", "VRChat")]
---
+++ AvatarStatsShowAuthor.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using MelonLoader;
+using UnhollowerRuntimeLib;
+
+namespace AvatarStatsShowAuthor
+{
+    public static class BuildInfo
+    {
+        public const string Name = "AvatarStatsShowAuthor";
+        public const string Author = "Herp Derpinstine, 404";
+        public const string Company = "Lava Gang";
+        public const string Version = "1.0.0";
+        public const string DownloadLink = "https://github.com/HerpDerpinstine/AvatarStatsShowAuthor";
+    }
+
+    public class AvatarStatsShowAuthor : MelonMod
+    {
+        private static VRC.UI.PageAvatar pageAvatar;
+        private static VRC.UI.PageUserInfo pageUserInfo;
+
+        public override void VRChat_OnUiManagerInit()
+        {
+            GameObject pageUserInfoObj = GameObject.Find("UserInterface/MenuContent/Screens/UserInfo");
+            if (pageUserInfoObj != null)
+            {
+                pageUserInfo = pageUserInfoObj.GetComponent<VRC.UI.PageUserInfo>();
+                if (pageUserInfo != null)
+                {
+                    pageAvatar = Resources.FindObjectsOfTypeAll<VRC.UI.PageAvatar>().First(p => (p.transform.Find("Change Button") != null));
+                    if (pageAvatar != null)
+                    {
+                        GameObject statspopup = GameObject.Find("UserInterface/MenuContent/Popups/AvatarStatsPopup");
+                        if (statspopup != null)
+                        {
+                            Transform documentationbutton = statspopup.transform.Find("AvatarStatsMenu/_Buttons/DocumentationButton");
+                            if (documentationbutton != null)
+                            {
+                                RectTransform recttrans = documentationbutton.GetComponent<RectTransform>();
+                                if (recttrans != null)
+                                {
+                                    recttrans.sizeDelta = new Vector2((recttrans.sizeDelta.x - 600), recttrans.sizeDelta.y);
+                                    recttrans.localPosition = new Vector3((recttrans.localPosition.x - 300), recttrans.localPosition.y, recttrans.localPosition.z);
+                                }
+
+                                Transform buttontxt_trans = documentationbutton.Find("Text");
+                                if (buttontxt_trans != null)
+                                {
+                                    Text buttontxt = buttontxt_trans.GetComponent<Text>();
+                                    if (buttontxt != null)
+                                        buttontxt.fontSize -= 10;
+                                }
+
+                                Transform buttonoverheadtxt_trans = documentationbutton.Find("Text (1)");
+                                if (buttonoverheadtxt_trans != null)
+                                {
+                                    RectTransform recttranstxt = buttonoverheadtxt_trans.GetComponent<RectTransform>();
+                                    if (recttranstxt != null)
+                                    {
+                                        recttranstxt.sizeDelta = new Vector2((recttranstxt.sizeDelta.x + 600), recttranstxt.sizeDelta.y);
+                                        recttranstxt.localPosition = new Vector3((recttranstxt.localPosition.x + 300), recttranstxt.localPosition.y, recttranstxt.localPosition.z);
+                                    }
+                                }
+
+                                Transform showcreatorbutton_trans = DuplicateButton(documentationbutton, "Show Avatar Author", new Vector2(600, 0));
+                                Button showcreatorbutton = showcreatorbutton_trans.GetComponent<Button>();
+                                showcreatorbutton.onClick = new Button.ButtonClickedEvent();
+                                showcreatorbutton.onClick.AddListener(DelegateSupport.ConvertDelegate<UnityAction>(new Action(() => {
+                                    if ((pageUserInfo != null) && (pageAvatar != null) && (pageAvatar.avatar != null))
+                                    {
+                                        VRC.Core.ApiAvatar currentApiAvatar = pageAvatar.avatar.field_Internal_ApiAvatar_0;
+                                        if (currentApiAvatar != null)
+                                        {
+                                            string authorid = currentApiAvatar.authorId;
+                                            if (!string.IsNullOrEmpty(authorid))
+                                            {
+                                                HideCurrentPopup();
+                                                VRCUiManager.prop_VRCUiManager_0.Method_Public_Void_Boolean_0(true);
+                                                VRC.Core.APIUser.FetchUser(authorid, new Action<VRC.Core.APIUser>((user) =>
+                                                {
+                                                    QuickMenu.prop_QuickMenu_0.prop_APIUser_0 = user;
+                                                    QuickMenu.prop_QuickMenu_0.Method_Public_Void_Int32_Boolean_0(4, false);
+                                                }), null);
+                                            }
+                                        }
+                                    }
+                                })));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void HideCurrentPopup()
+        {
+            if (!VRCUiManager.prop_VRCUiManager_0.prop_Dictionary_2_String_VRCUiPage_0.ContainsKey("POPUP"))
+                return;
+            VRCUiPage item = VRCUiManager.prop_VRCUiManager_0.prop_Dictionary_2_String_VRCUiPage_0["POPUP"];
+            item.gameObject.SetActive(false);
+            if (item.onPageDeactivated != null)
+                item.onPageDeactivated.Invoke();
+            VRCUiManager.prop_VRCUiManager_0.prop_Dictionary_2_String_VRCUiPage_0.Remove("POPUP");
+        }
+
+        private static Transform DuplicateButton(Transform baseButton, string buttonText, Vector2 posDelta)
+        {
+            GameObject buttonGO = new GameObject("DuplicatedButton", new Il2CppSystem.Type[] {
+                Il2CppType.Of<Button>(),
+                Il2CppType.Of<Image>()
+            });
+
+            RectTransform rtO = baseButton.GetComponent<RectTransform>();
+            RectTransform rtT = buttonGO.GetComponent<RectTransform>();
+
+            buttonGO.transform.SetParent(baseButton.parent);
+            buttonGO.GetComponent<Image>().sprite = baseButton.GetComponent<Image>().sprite;
+            buttonGO.GetComponent<Image>().type = baseButton.GetComponent<Image>().type;
+            buttonGO.GetComponent<Image>().fillCenter = baseButton.GetComponent<Image>().fillCenter;
+            buttonGO.GetComponent<Button>().colors = baseButton.GetComponent<Button>().colors;
+            buttonGO.GetComponent<Button>().targetGraphic = buttonGO.GetComponent<Image>();
+
+            rtT.localScale = rtO.localScale;
+
+            rtT.anchoredPosition = rtO.anchoredPosition;
+            rtT.sizeDelta = rtO.sizeDelta;
+
+            rtT.localPosition = rtO.localPosition + new Vector3(posDelta.x, posDelta.y, 0);
+            rtT.localRotation = rtO.localRotation;
+
+            GameObject textGO = new GameObject("Text", new Il2CppSystem.Type[] { Il2CppType.Of<Text>() });
+            textGO.transform.SetParent(buttonGO.transform);
+
+            RectTransform rtO2 = baseButton.Find("Text").GetComponent<RectTransform>();
+            RectTransform rtT2 = textGO.GetComponent<RectTransform>();
+            rtT2.localScale = rtO2.localScale;
+
+            rtT2.anchorMin = rtO2.anchorMin;
+            rtT2.anchorMax = rtO2.anchorMax;
+            rtT2.anchoredPosition = rtO2.anchoredPosition;
+            rtT2.sizeDelta = rtO2.sizeDelta;
+
+            rtT2.localPosition = rtO2.localPosition;
+            rtT2.localRotation = rtO2.localRotation;
+
+            Text tO = baseButton.Find("Text").GetComponent<Text>();
+            Text tT = textGO.GetComponent<Text>();
+            tT.text = buttonText;
+            tT.font = tO.font;
+            tT.fontSize = tO.fontSize;
+            tT.fontStyle = tO.fontStyle;
+            tT.alignment = tO.alignment;
+            tT.color = tO.color;
+
+            return buttonGO.transform;
+        }
+    }
+}
+ 
