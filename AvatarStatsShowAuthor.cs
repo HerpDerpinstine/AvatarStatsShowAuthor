71386ff4b2bce758256b3e7e5260775ef82b554e
Herp Derpinstine <herpderpinstine@gmail.com>
2020-09-09 01:25:11 -0500

    Update AvatarStatsShowAuthor.cs

--- AvatarStatsShowAuthor.cs
+++ AvatarStatsShowAuthor.cs
@@ -12,9 +12,9 @@ namespace AvatarStatsShowAuthor
     public static class BuildInfo
     {
         public const string Name = "AvatarStatsShowAuthor";
-        public const string Author = "Herp Derpinstine, 404";
+        public const string Author = "Herp Derpinstine & dave-kun";
         public const string Company = "Lava Gang";
-        public const string Version = "1.0.1";
+        public const string Version = "1.0.2";
         public const string DownloadLink = "https://github.com/HerpDerpinstine/AvatarStatsShowAuthor";
     }
 
